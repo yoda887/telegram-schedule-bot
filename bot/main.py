e818f79a9f98edaad09b13396bc826861b5093fa
yoda887 <59238117+yoda887@users.noreply.github.com>
2025-05-02 14:47:57 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,1 +1,10 @@
+import asyncio
+from fastapi import FastAPI
+from bot import dp, bot
+
+app = FastAPI()
+
+@app.on_event("startup")
+async def on_startup():
+    asyncio.create_task(dp.start_polling(bot))
 
